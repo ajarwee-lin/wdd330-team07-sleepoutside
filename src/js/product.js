3e5e6f528073d6cf49a7178db4194ecd46a91b6f
LizandroV <61571830+LizandroV@users.noreply.github.com>
2024-06-22 10:45:34 -0500

    W02 Team Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,30 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = [];
-  if (getLocalStorage("so-cart") === null){
-    setLocalStorage("so-cart", cartItems)
-  }else{
-    getLocalStorage("so-cart").map((item) => cartItems.push(item));
-  }
-  cartItems.push(product)
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// function addProductToCart(product) {
+//   const cartItems = [];
+//   if (getLocalStorage("so-cart") === null){
+//     setLocalStorage("so-cart", cartItems)
+//   }else{
+//     getLocalStorage("so-cart").map((item) => cartItems.push(item));
+//   }
+//   cartItems.push(product)
+//   setLocalStorage("so-cart", cartItems);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
